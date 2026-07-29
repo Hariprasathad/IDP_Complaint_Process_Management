6e40684665d87a5b52c0560c24322d1e0b42c318
Hariprasath AD <hariprasath.ad@idp.com>
2026-07-29 17:59:29 +0530

    boder colour and animation

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -3,12 +3,8 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="bg-white relative z-10 w-full">
-      <div className="w-full max-w-[824px] mx-auto px-[40px]">
-        <div className="flex justify-start items-center h-[72px]">
-          <div className="flex-shrink-0 flex items-center">
-            <img src="/idplogo.png" alt="IDP Education" className="h-[40px] w-auto" />
-          </div>
-        </div>
+      <div className="w-full">
+        <img src="/idplogo.png" alt="IDP Education" className="h-[40px] w-auto mt-[14px] ml-[34px] mb-[14px]" />
       </div>
       {/* Subtle gradient shadow below header */}
       <div className="absolute left-0 right-0 bottom-[-8px] h-[8px] pointer-events-none bg-gradient-to-b from-black/[0.04] to-transparent" />
